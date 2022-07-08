9ee8ae870b0022d98c73cf24d0c8fb8a79f68344
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-07-08 12:51:02 +0300

    01gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+const imageCardList = createImageMarkup(galleryItems)
+
+galleryDiv.insertAdjacentHTML ('beforeend', imageCardList);
+
+function createImageMarkup(galleryItems) {
+    return galleryItems
+        .map(({preview, description}) => {
+            return `<li class="image-card"> <img src=" ${preview}" alt= "${description}" width= 350px /></li>`
+        }
+        )
+        .join('');
+}
+
+
+// додає збільшений розмір картинки
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
