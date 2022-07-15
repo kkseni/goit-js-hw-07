5825fd75159b82cf06e78165a68fc63679a2aa35
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-07-15 13:44:52 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,6 @@
 import { galleryItems } from './gallery-items.js';
+import * as basicLightbox  from './basiclightbox.js'
+
 // Change code below this line
 
 console.log(galleryItems);
@@ -17,8 +19,30 @@ function createImageMarkup(galleryItems) {
         .join('');
 }
 
+const divGallery = document.querySelector('.gallery');
+const selectedEl = new Set();
 
-// додає збільшений розмір картинки
+divGallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    const nextActiveImg = evt.target;
+
+    console.log(nextActiveImg.classList.contains('.image-card'));
+
+    nextActiveImg.classList.toggle('.image-card');
+    selectedEl.add(nextActiveImg.dataset.source);
+    
+    const instance = basicLightbox.create(`
+    <img src="nextActiveImg.dataset.source" width="800" height="600">`)
+    instance.show()
+}
+
+
+ // import * as basicLightbox from 'basiclightbox'
+
 // const instance = basicLightbox.create(`
 //     <img src="assets/images/image.png" width="800" height="600">
 // `)
---
+++ js/basiclightbox.js
@@ -0,0 +1,1 @@
+'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js'
